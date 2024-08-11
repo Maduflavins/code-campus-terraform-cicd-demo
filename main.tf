1dbecd9240fdad8a255e47b892f6e0b4ed03b05a
MADUABUCHI <maduabuchiokonkwo@gmail.com>
2024-08-11 19:45:39 +0100

    destroy

--- main.tf
+++ main.tf
@@ -11,42 +11,42 @@ provider "google" {
   project = var.project
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "codecampus-network"
-}
+# resource "google_compute_network" "vpc_network" {
+#   name = "codecampus-network"
+# }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "codecampus-instance"
-  machine_type = "f1-micro"
-  zone         = var.zone
-  tags         = ["web", "dev"]
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "codecampus-instance"
+#   machine_type = "f1-micro"
+#   zone         = var.zone
+#   tags         = ["web", "dev"]
 
-  boot_disk {
-    initialize_params {
-      #image = "debian-cloud/debian-11"
-      image = "cos-cloud/cos-stable"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       #image = "debian-cloud/debian-11"
+#       image = "cos-cloud/cos-stable"
+#     }
+#   }
 
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
-resource "google_compute_firewall" "code-campus-firewal" {
-  name    = "code-campus-firewal"
-  network = google_compute_network.vpc_network.name
+#   network_interface {
+#     network = google_compute_network.vpc_network.name
+#     access_config {
+#     }
+#   }
+# }
+# resource "google_compute_firewall" "code-campus-firewal" {
+#   name    = "code-campus-firewal"
+#   network = google_compute_network.vpc_network.name
 
-  allow {
-    protocol = "icmp"
-  }
+#   allow {
+#     protocol = "icmp"
+#   }
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["80"]
+#   }
 
-  source_tags = ["web"]
-}
+#   source_tags = ["web"]
+# }
 
